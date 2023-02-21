c404f212fb0e07467e0d5030eea83960ca984dc8
Fredrick-Samuel <fredricksamuel379@gmail.com>
2023-02-21 12:11:32 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,13 +12,13 @@ int a, b, c, d;
 
 for (a = 0; a <= 2; a++)
 {
-for (b = 0; b <= 2; b++)
+for (b = 0; b <= 9; b++)
 {
 if  ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 {
-for (c = 0; c <= 2; c++)
+for (c = 0; c <= 5; c++)
 {
-for (d = 0; d <= 2; d++)
+for (d = 0; d <= 9; d++)
 {
 _putchar(a + '0');
 _putchar(b + '0');
